a27a6575de0b87f9c57a31727f766954abf94757
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2023-07-11 20:10:31 -0300

    Adicionado botão para mobile

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,11 @@
 if (window.SimpleAnime) {
   new SimpleAnime();
 }
+
+const botao = document.getElementById("mobile-button");
+
+botao.addEventListener("click", function () {
+  const menu = document.getElementById("menu-mobile");
+
+  menu.classList.toggle("open");
+});
