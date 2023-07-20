a43e96cf80df3803e9ec35acce561cdd05183321
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2023-07-19 21:44:55 -0300

    testando modal

--- JS/script.js
+++ JS/script.js
@@ -9,3 +9,18 @@ botao.addEventListener("click", function () {
 
   menu.classList.toggle("open");
 });
+
+const modal = document.getElementById("modal");
+const btn = document.getElementById("fechar");
+
+btn.addEventListener("click", function () {
+  if (modal) {
+    modal.classList.toggle("show");
+  }
+});
+
+function exibirPopup() {
+  modal.classList.add("show");
+}
+const tempoAtraso = 2000;
+setTimeout(exibirPopup, tempoAtraso);
