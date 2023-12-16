03c9b13ce2abcc4a6c249fc7d08a4bc6589fef11
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2023-12-16 18:05:51 -0300

    adding about me section

--- my-portfolio/components/About.tsx
+++ my-portfolio/components/About.tsx
@@ -13,8 +13,24 @@ export default function About() {
       transition={{ delay: 0.175 }}
     >
       <SectionHeading>Sobre mim</SectionHeading>
-      <p className="mb-3"></p>
-      <p></p>
+      <p className="mb-3">
+        Sou apaixonado por transformar ideias criativas em experiências
+        digitais. Sou desenvolvedor front-end dedicado com conhecimento em
+        tecnologias web modernas. Possuo conhecimentos em{" "}
+        <span className="font-bold">
+          React (Next.js), JavaScript e TypeScript
+        </span>
+        . Estou sempre atento às últimas atualizações e melhores práticas do
+        desenvolvimento front-end, e estou pronto para contribuir de maneira
+        significativa para o sucesso do seu time. Se você está em busca de um
+        desenvolvedor front-end{" "}
+        <span className="font-medium">
+          apaixonado por inovação e comprometido com a entrega de produtos de
+          alta qualidade
+        </span>
+        , ficarei feliz em discutir como minhas habilidades podem agregar valor
+        à sua equipe. <span className="font-bold">Vamos juntos!</span>
+      </p>
     </motion.section>
   );
 }
