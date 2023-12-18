88f9242d3711a9eadba2b60bac7a0abe78964ed7
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2023-12-18 16:06:31 -0300

    Commit

--- my-portfolio/components/Project.tsx
+++ my-portfolio/components/Project.tsx
@@ -31,8 +31,8 @@ export default function Project({
       }}
       className="group mb-3 sm:mb-8 last:mb-0"
     >
-      <section className=" bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition  rounded-lg">
-        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w[50%] flex flex-col  h-full group-even:ml-[18rem] sm:group-even:pl-8 group-odd:mr-[18rem]">
+      <section className=" bg-gray-100 sm:max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition rounded-lg">
+        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 w-[100%] sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem] sm:group-even:pl-8 group-odd:mr-[18rem]">
           <h3 className="text-2xl font-semibold">{title}</h3>
           <p className="mt-2 leading-relaxed text-gray-700 ">{description}</p>
           <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
@@ -51,7 +51,7 @@ export default function Project({
           src={imageUrl}
           alt={title}
           quality={95}
-          className="absolute bottom-0 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl group-even:right-[initial] group-even:-left-40 group-hover:translate-x-3 group-hover: translate-y-3 group-hover:-rotate-2 group-hover:scale-[1.04] transition 
+          className="absolute sm:bottom-0 sm:-right-40 sm:w-[28.25rem] rounded-t-lg shadow-2xl group-even:right-[initial] group-even:-left-40 group-hover:translate-x-3 group-hover: translate-y-3 group-hover:-rotate-2 group-hover:scale-[1.04] transition 
           group-even:group-hover:translate-x-3
           group-even:group-hover:translate-y-3
           group-even:group-hover:rotate-2 
